b6ed890f075b6161688e5aafb2d235108f89665b
simon gould <simon.joseph.gould@gmail.com>
2017-11-06 00:46:45 +0000

    basic lambda that prints out event, sam template and script to deploy

---
+++ deploy-lambda.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd sonar-scan && npm install
+
+cd ../
+
+aws cloudformation package \
+  --template-file iac_config/template.yaml \
+  --s3-bucket sonar-scan-lambda-code \
+  --output-template-file iac_config/packaged-template.yaml
+
+aws cloudformation deploy \
+    --template-file iac_config/packaged-template.yaml \
+    --stack-name sonar-scan-lambda-stack \
+    --capabilities CAPABILITY_IAM
