06d20f628249375f111b26f78e3fdbbe6d71acf4
Gould <simon.gould@metoffice.gov.uk>
2017-11-08 15:59:55 +0000

    more mini refactor while waiting for  pipeline to build

--- sonar-scan/src/sonarScanLambda.js
+++ sonar-scan/src/sonarScanLambda.js
@@ -1,6 +1,12 @@
 exports.handler = function(event, context, callback) {
    console.log("event = %j", event);
    function validateEvent() {
+      if (event === null){
+          throw new Error('event can not be null');
+      }
+       if (event === undefined){
+           throw new Error('event can not be undefined');
+       }
       if (!event.hasOwnProperty("CodePipeline.job")) {
          throw new Error('Trigger event must be a code pipeline event');
       }
--- sonar-scan/src/sonarScanLambda.test.js
+++ sonar-scan/src/sonarScanLambda.test.js
@@ -11,15 +11,43 @@ describe('sonar-scan event handler', function () {
         eventHandler = require('./sonarScanLambda');
     });
 
-    it("should should throw an error if the event is not a code pipeline event", function () {
-        eventHandler.handler(
-            {"blah": "blah blah"},
-            undefined,
-            (err, data)=>{
-                expect(err).to.be.an('error');
-                expect(err.message).to.equal('Trigger event must be a code pipeline event');
-                expect(data).to.be.a('null');
-            }
-        )
+    describe("should should throw an error if the event is not a code pipeline event", function () {
+
+        it("will set error on callback if json event is not correct", function(){
+            eventHandler.handler(
+                {"blah": "blah blah"},
+                undefined,
+                (err, data)=>{
+                    expect(err).to.be.an('error');
+                    expect(err.message).to.equal('Trigger event must be a code pipeline event');
+                    expect(data).to.be.a('null');
+                }
+            );
+        });
+
+        it("will set error on callback if event is null", function(){
+            eventHandler.handler(
+                null,
+                undefined,
+                (err, data)=>{
+                    expect(err).to.be.an('error');
+                    expect(err.message).to.equal('event can not be null');
+                    expect(data).to.be.a('null');
+                }
+            );
+        });
+
+        it("will set error on callback if event is undefined", function(){
+            eventHandler.handler(
+                undefined,
+                undefined,
+                (err, data)=>{
+                    expect(err).to.be.an('error');
+                    expect(err.message).to.equal('event can not be undefined');
+                    expect(data).to.be.a('null');
+                }
+            );
+        });
+
     });
 });
