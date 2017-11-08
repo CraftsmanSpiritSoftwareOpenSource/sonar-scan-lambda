88c336ec9311eca71e9cf779039f274422c84a5a
Gould <simon.gould@metoffice.gov.uk>
2017-11-08 17:48:59 +0000

    changed throw behavior to use call back to make it more node-tastic

--- sonar-scan/src/sonarScanLambda.js
+++ sonar-scan/src/sonarScanLambda.js
@@ -1,20 +1,28 @@
+const validateEvent = (event, callback) => {
+    if (event === null){
+       return callback(new Error('event can not be null'));
+    }
+    if (event === undefined){
+       return callback(new Error('event can not be undefined'));
+    }
+    if (!event.hasOwnProperty("CodePipeline.job")) {
+       return callback(new Error('Trigger event must be a code pipeline event'));
+    }
+    callback()
+}
+
 exports.handler = function(event, context, callback) {
-   console.log("event = %j", event);
-   function validateEvent() {
-      if (event === null){
-          throw new Error('event can not be null');
-      }
-       if (event === undefined){
-           throw new Error('event can not be undefined');
-       }
-      if (!event.hasOwnProperty("CodePipeline.job")) {
-         throw new Error('Trigger event must be a code pipeline event');
-      }
-   }
 
    try {
-      validateEvent();
-      return callback(null, "logged the event");
+      validateEvent(event, (err)=>{
+         "use strict";
+         if (err){
+            return callback(err, null);
+         }
+         console.log("event = %j", event);
+         return callback(null, "logged the event");
+      });
+
    } catch (err) {
       console.error(err)
       return callback(err, null)
