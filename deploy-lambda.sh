81792590b67f083affb2126b8ecd2a6ee979d6c2
Gould <simon.gould@metoffice.gov.uk>
2017-11-20 12:48:58 +0000

    tidied up and made clearer the deploy script

--- deploy-lambda.sh
+++ deploy-lambda.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+
+USAGE_STRING="Usage ./deploy-lambda.sh <s3-bucket-name>"
+
+if [[ $# != 1 ]] ; then
+    echo $USAGE_STRING
+    exit 1
+fi
+
+s3_bucket_name=$1
+
 cd sonar-scan && npm install
 
 cd ../
 
 aws cloudformation package \
   --template-file iac_config/template.yaml \
-  --s3-bucket met-sonar-scan-lambda-code \
+  --s3-bucket ${s3_bucket_name} \
   --output-template-file iac_config/packaged-template.yaml
 
 aws cloudformation deploy \
